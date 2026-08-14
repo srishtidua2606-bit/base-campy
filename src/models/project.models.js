1523fc623eb6724ba9a7ed2e392afb302949746f
Srishti Dua <Srishtidua2606@gmail.com>
2026-08-14 17:39:59 +0530

    feat: Add Mongoose models for Project, Note, Task, Subtask, and ProjectMember

---
+++ src/models/project.models.js
@@ -0,0 +1,25 @@
+import mongoose , { Schema } from "mongoose"
+
+const projectSchema = new Schema({
+
+    name : {
+        type : String,
+        required : true,
+        unique : true,
+        trim: true,
+      
+    },
+    description:{
+        type: String,
+        
+    },
+    createdBy : {
+        type : Schema.Types.ObjectId, // i wil refer to another document.
+        ref : "User",
+        required : true
+    }
+
+}, { timestamps:true}
+);
+
+export const Project = mongoose.model("Project", projectSchema)
---
+++ src/models/projectmember.models.js
@@ -0,0 +1,24 @@
+import mongoose , { Schema } from "mongoose"
+import {AvailableUserRole , UserRolesEnum} from "../utils/constants.js"
+
+const projectMemberSchema = new Schema({
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required : true
+    },
+    project : {
+        type : Schema.Types.ObjectId,
+        ref: "Project",
+        required:true
+    },
+    role : {
+        type : String,
+        enum : AvailableUserRole,
+        default : UserRolesEnum.MEMBER
+    }
+}, {
+    timestamps:true
+})
+
+export const projectMember = mongoose.model("ProjectMember", projectMemberSchema)
