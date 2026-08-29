dfd173c6b2847998af177ad5b4b7d1a94b9594c7
Srishti Dua <Srishtidua2606@gmail.com>
2026-08-30 03:35:36 +0530

    fix: Correct indentation in user schema for consistency

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -54,7 +54,7 @@ const userSchema = new Schema(
             type : String
         } , 
         forgotPasswordExpiry : {
-                type : Date
+            type : Date
         },
         emailVerificationToken : {
             type : String
